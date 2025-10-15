2a50180fd5b46b2b65f68d23545879a1e568d4bb
juliettedef <juliette.defrance@outlook.fr>
2025-10-15 11:23:04 +0200

    ajout de la fonction playGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// fonction powerPC
+
 function powerPc() {
     const pouvoirs = ["feu", "eau", "terre"];
     const random = Math.floor(Math.random() * pouvoirs.length);
@@ -6,17 +8,39 @@ function powerPc() {
 
 console.log("Pouvoir du PC :", powerPc());
 
+// fonction powerPlayer
+
 function powerPlayer(event) {
     const playerChoice = event.target.id;
     console.log("Le joueur a cliqué sur :", playerChoice);
     return playerChoice;
 }
 
+// récupération des clics sur les boutons
+
 const buttons = document.querySelectorAll(".buttons button");
 
 buttons.forEach(button => {
     button.addEventListener("click", powerPlayer);
 });
+
+// fonction playGame
+
+function playGame(playerChoice, pcChoice) {
+    let resultat = "";
+
+    if (playerChoice === pcChoice) {
+        resultat = `Égalité ! Vous avez tous les deux choisi ${playerChoice}.`;
+    } else if (
+        (playerChoice === "feu" && pcChoice === "terre") ||
+        (playerChoice === "eau" && pcChoice === "feu") ||
+        (playerChoice === "terre" && pcChoice === "eau")
+    ) {
+        resultat = `Bravo ! ${playerChoice} bat ${pcChoice}.`;
+    } else {
+        resultat = `Perdu ! ${pcChoice} bat ${playerChoice}.`;
+    }
+}
 
 
 
