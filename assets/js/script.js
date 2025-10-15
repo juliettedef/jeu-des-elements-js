117fdfb7bf2ecf560ed38d2ec405a5b830573f3f
juliettedef <juliette.defrance@outlook.fr>
2025-10-15 10:37:31 +0200

    récupération du clic du joueur

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-
-
-const pouvoirs = ["feu", "eau", "terre"];
-function powerPc () {
+function powerPc() {
+    const pouvoirs = ["feu", "eau", "terre"];
     const random = Math.floor(Math.random() * pouvoirs.length);
     return pouvoirs[random];
 }
 
 console.log("Pouvoir du PC :", powerPc());
+
+const buttons = document.querySelectorAll(".buttons button");
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const playerChoice = event.target.id;
+        console.log("Le joueur a cliqué sur :", playerChoice);
+    });
+});
+
+
+
+
